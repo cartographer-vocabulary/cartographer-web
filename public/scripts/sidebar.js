4eb10da09f1e3fb2635c266cebace8ccacbcbd16
Tony Zhang <tony.j.h.zhang@gmail.com>
2021-02-15 08:03:00 -0500

    a bit more icons and stuff

--- public/scripts/sidebar.js
+++ public/scripts/sidebar.js
@@ -4,7 +4,20 @@ function updateLists(querySnapshot){
     //convert array into html basically
     let items = sorted.map(doc => {
                                                 //this part changes the url when you click and calles update window function in content.js
-        return(doc.exists ? `<li onclick='window.history.pushState("","","/list/${doc.id}");updateWindows()'> ${doc.data().name} </li>` : "  ")
+        return(doc.exists ? `
+            <li class = "horizontal" onclick='window.history.pushState("","","/list/${doc.id}");updateWindows()'>
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-list" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <line x1="9" y1="6" x2="20" y2="6" />
+                    <line x1="9" y1="12" x2="20" y2="12" />
+                    <line x1="9" y1="18" x2="20" y2="18" />
+                    <line x1="5" y1="6" x2="5" y2="6.01" />
+                    <line x1="5" y1="12" x2="5" y2="12.01" />
+                    <line x1="5" y1="18" x2="5" y2="18.01" />
+                </svg>
+               <span> ${doc.data().name} </span> 
+            </li>
+        ` : "  ")
     })
     document.getElementById("my-lists-container").innerHTML=items.join(' ');
 }
