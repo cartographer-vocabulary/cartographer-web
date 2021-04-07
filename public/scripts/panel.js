19cd5296ff80fab3b7691b8c292410cca008900c
Tony Zhang <tony.j.h.zhang@gmail.com>
2021-04-07 19:11:52 -0400

    slight fade-in

--- public/scripts/panel.js
+++ public/scripts/panel.js
@@ -11,31 +11,34 @@ let folderSettingsPanel = document.getElementById("folder-settings-panel");
 let folderSettingsBtn = document.getElementById("folder-settings");
 //the three functions are basically duplicates and shows the corresponding panel when button is clicked
 signInBtn.addEventListener("click", ()=>{
-    panelContainer.style.display = "flex";
-    signInPanel.style.display = "block";
+    // panelContainer.style.display = "flex";
+    // signInPanel.style.display = "block";
+    panelContainer.classList.remove("hidden");
+    signInPanel.classList.remove("hidden");
+
 })
 
 profileBtn.addEventListener("click", ()=>{
-    panelContainer.style.display = "flex";
-    profilePanel.style.display = "block";
+    panelContainer.classList.remove("hidden");
+    profilePanel.classList.remove("hidden");
 })
 
 listSettingsBtn.addEventListener("click", ()=>{
-    panelContainer.style.display = "flex";
-    listSettingsPanel.style.display = "block";
+    panelContainer.classList.remove("hidden");
+    listSettingsPanel.classList.remove("hidden");
 })
 
 folderSettingsBtn.addEventListener("click", ()=>{
-    panelContainer.style.display = "flex";
-    folderSettingsPanel.style.display = "block";
+    panelContainer.classList.remove("hidden");
+    folderSettingsPanel.classList.remove("hidden");
 })
 
 //hides the panels when you click outside
 panelContainer.addEventListener("click", (e)=>{
     if(e.target == panelContainer) {
-        panelContainer.style.display = "none";
+        panelContainer.classList.add("hidden");
         for (let panel of panelContainer.children) {
-            panel.style.display = "none";
+            panel.classList.add("hidden");
         }
     }
 })
