96abd6cb96c1c6cbb077789cc67e7e4e78fdda91
Tony Zhang <61944932+Tony1324@users.noreply.github.com>
2021-07-20 11:04:13 -0400

    auth

--- public/scripts/auth.js
+++ public/scripts/auth.js
@@ -1,56 +1,27 @@
 //variable for firestore
 var firestore = firebase.firestore();
 
-//I wrapped it in a function so you can't access this variables from other files, which might cuase conflict.
-(function() {
-let signInBtn = document.getElementById("login-btn");
-let profileBtn = document.getElementById("open-profile-btn");
+
 //elements that need to be replaced with the users name, such as in the profile button or profile panel
-let fullnameElements = document.getElementsByClassName("user-full-name");
-let signOutBtn = document.getElementById("signout-btn");
-let unsubscribeLists;
-let unsubscribeFolders;
+
+let subscriptions = {}
+
 window.appLogin = false
-//kinda copy and pasted from somewhere
+
 function initApp() {
     //detects when the auth state changes
-    firebase.auth().onAuthStateChanged(function(user) {
+    firebase.auth().onAuthStateChanged(function(_user) {
 
         //if there is an user, it means they are signed in, else they are signed out.
-        if (user) {
-            //hides the sign in button and shows profile button when signed in
-            profileBtn.style.display = "block";
-            signInBtn.style.display = "none";
-            //gets the data of stuff and stores it in window, which is basically like defining a global variable
-            window.displayName = user.displayName;
-            window.userEmail = user.email;
-            window.emailVerified = user.emailVerified;
-            window.photoURL = user.photoURL;
-            window.uid = user.uid;
-            var phoneNumber = user.phoneNumber;
-            var providerData = user.providerData;
-            updateUserInfo()
-            //creates/updates user document
-            firestore.collection('users').doc(uid).get().then(doc => {
-                if(!doc.exists) {
-                    firestore.collection('users').doc(uid).set({
-                        displayName: displayName,
-                        email: userEmail
-                    }, {merge: true}).catch((error) => {
-                        console.error("error:" + error)
-                    })
-                }else{
-                    firestore.collection('users').doc(uid).set({
-                        email: userEmail
-                    }, {merge: true}).catch(error => {console.log(error)})
-                }
-            })
+        if (_user) {
 
-            unsubscribeLists = firestore.collection('lists')
-                .where(`roles.${uid}`,'in',['creator','editor','viewer'])
+            updateUserInfo(_user)
+
+            subscriptions.sidebarLists = firestore.collection('lists')
+                .where(`roles.${_user.uid}`,'in',['creator','editor','viewer'])
                 .onSnapshot(updateLists);
-            unsubscribeFolders = firestore.collection('folders')
-                .where(`roles.${uid}`,'in',['creator','editor','viewer'])
+            subscriptions.siderbarFolders = firestore.collection('folders')
+                .where(`roles.${_user.uid}`,'in',['creator','editor','viewer'])
                 .onSnapshot(updateFolders);
             
             if(appLogin){
@@ -58,51 +29,69 @@ function initApp() {
                 window.location.replace(`cartographer://idtoken/${idToken}`)
                 })       
             }
+
         } else {
-            // User is signed out. and makes them null so you can't have these variables
-            window.displayName = null;
-            window.userEmail = null;
-            window.emailVerified = null;
-            window.photoURL = null;
-            window.uid = null;
-            //hides profile button and shows log in button
-            profileBtn.style.display = "none";
-            signInBtn.style.display = "block";
-            //changes the text with the user's name to "profile"
-            for(let fullnameElement of fullnameElements){
-                fullnameElement.innerHTML = "Profile"
-            }
-            //unsubscribe
-            unsubscribeLists?.()
-            unsubscribeFolders?.()
-            //clears the lists on the sidebar
-            clearLists();
-            clearFolders();
-            if(appLogin){
-                document.querySelector("#panel-container").classList.remove("hidden")
-                document.querySelector("#sign-in-panel").classList.remove("hidden")
-            }
+            clearUserInfo()
         }
     }, function(error) {
         console.log(error);
     });
 };
 
-//when the sign out button is clicked inside the profile panel
-signOutBtn.addEventListener('click', ()=>{
-    //signed out using firebase
-    firebase.auth().signOut()
-        .catch(error => {
-            console.log("error when signing out:" + error);
-        })
-    //hides the panel when you click signout so you don't need to click outside because it's useless now that you have signed out
+function updateUserInfo(_user){
+    window.user = _user
+
+    let elementUpdates = () => {
+        
+        let signInBtn = document.getElementById("login-btn");
+        let profileBtn = document.getElementById("open-profile-btn");
+
+        profileBtn.style.display = "block";
+        signInBtn.style.display = "none";
+
+        let nameElements = document.getElementsByClassName("user-full-name");
+        for(let nameElement of nameElements){
+            nameElement.innerHTML = _user.displayName
+        }
+
+    }
+    
+    elementUpdates()
+    window.addEventListener("load", elementUpdates)
+    
+}
+
+function clearUserInfo(){
+    window.user = null
+
+    let signInBtn = document.getElementById("login-btn");
+    let profileBtn = document.getElementById("open-profile-btn");
+
+    profileBtn.style.display = "none";
+    signInBtn.style.display = "block";
+
+    let nameElements = document.getElementsByClassName("user-full-name");
+    for(let nameElement of nameElements){
+        nameElement.innerHTML = "Profile"
+    }
+    document.getElementById('my-lists-container').innerHTML = "";
+    document.getElementById("my-folders-container").innerHTML = "";
+
     let panelContainer = document.getElementById("panel-container");
     panelContainer.style.display = "none";
     for (let panel of panelContainer.children) {
         panel.style.display = "none";
     }
-})
+
+    subscriptions.sidebarLists?.()
+    subscriptions.sidebarFolders?.()
+}
+
+function signOut(){
+    firebase.auth().signOut()
+        .catch(error => {
+            console.log("error when signing out:" + error);
+        })
+}
 
 initApp()
-
-})();
